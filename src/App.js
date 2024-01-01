08fe93cb7d1c7cbac80e4689d35ff259d06c0e28
Lee Sol <leeso4567@naver.com>
2024-01-01 15:27:17 +0900

    일기 데이터 추가하기 기능 생성, App.js 로 State 끌어올리기 사용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useState, useRef } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current, //useRef(0) 의 객체이므로 current 는 0이 들어있음.
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
