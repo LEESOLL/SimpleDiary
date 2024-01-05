663863f03e7d80c20b8fe0a0c9737563f030eb4f
Lee Sol <leeso4567@naver.com>
2024-01-05 14:43:01 +0900

    React.memo 테스트 진행

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useRef, useEffect, useMemo } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
+// import OptimizeTest from "./OptimizeTest";
 
 function App() {
   const [data, setData] = useState([]);
@@ -68,6 +69,7 @@ function App() {
 
   return (
     <div className="App">
+      {/* <OptimizeTest /> */}
       <DiaryEditor onCreate={onCreate} />
       <div>전체 일기 : {data.length}</div>
       <div>기분 좋은 일기 개수 : {goodCount}</div>
